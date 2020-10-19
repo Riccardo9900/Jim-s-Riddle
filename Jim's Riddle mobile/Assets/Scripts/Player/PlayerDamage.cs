906fe0a14f6c2d965c8552510c25c480375f9f24
framain <63399271+framain@users.noreply.github.com>
2020-10-19 18:10:59 +0200

    prove
    Prove su mappa, personaggio, collisioni e respawn

---
+++ Jim's Riddle mobile/Assets/Scripts/Player/PlayerDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject DeathPanel;
+    public int healt = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeathPanel.SetActive(false);
+    }
+
+    public void isHit()
+    {
+        if (healt == 0)
+        {
+            Game.gameManager.isDead();
+            DeathPanel.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
