5256e290d72142fe150c4dcd912ad862d0960d5a
framain <63399271+framain@users.noreply.github.com>
2020-10-19 18:45:23 +0200

    Il gioco si ferma quando il player colpisce il cubo rosso

--- Jim's Riddle mobile/Assets/Scripts/Player/PlayerDamage.cs
+++ Jim's Riddle mobile/Assets/Scripts/Player/PlayerDamage.cs
@@ -6,22 +6,35 @@ public class PlayerDamage : MonoBehaviour
 {
     [SerializeField]
     public GameObject DeathPanel;
+    public GameObject DeathText;
     public int healt = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         DeathPanel.SetActive(false);
+        DeathText.SetActive(false);
     }
 
-    public void isHit()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (healt == 0)
+        if(collision.tag == "DeathCube")
         {
-            Game.gameManager.isDead();
+            Debug.Log("Hai preso il cubo sbagliato");
             DeathPanel.SetActive(true);
+            DeathText.SetActive(true);
         }
     }
+
+
+   // public void isHit()
+   //{
+   //     if (healt == 0)
+   //     {
+   //        Game.gameManager.isDead();
+   //         DeathPanel.SetActive(true);
+   //     }
+   // }
 
     // Update is called once per frame
     void Update()
