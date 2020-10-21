de26d1ab4f5c1142a26b38acc81e5780d98111cf
Riccardo9900 <rikys8@hotmail.it>
2020-10-21 12:53:08 +0200

    Death and respawn

--- Jim's Riddle mobile/Assets/Scripts/CollisionWithFloor.cs
+++ Jim's Riddle mobile/Assets/Scripts/CollisionWithFloor.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class CollisionWithFloor : MonoBehaviour
 {
+    public GameObject canvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.canvas.SetActive(false);
     }
 
-    void OnTriggerEnter2D (Collider2D coll)
+    void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.tag == "Pavimento Sbagliato")
         {
-            Debug.Log("Collisione con pavimento sbagliato!");
+            this.canvas.SetActive(true);
+            GetComponent<PlayerMovement>().enabled = false;
+
         }
+    }
+
+    public void Respawn ()
+    {
+        GetComponent<PlayerMovement>().enabled = true;
+        this.canvas.SetActive(false);
+        gameObject.transform.position = new Vector3(0f, -9.2f, 0f);
+        
     }
 
     // Update is called once per frame
