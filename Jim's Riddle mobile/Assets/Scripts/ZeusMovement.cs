73a6c07c7428d17042ef6a53bd7a2932d395ec88
Riccardo9900 <rikys8@hotmail.it>
2020-11-04 16:49:57 +0100

    zeusMovement parziale

---
+++ Jim's Riddle mobile/Assets/Scripts/ZeusMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeusMovement : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public float movementIncrease;
+    private Vector3 vettoreMovimento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < 4 ) {
+            vettoreMovimento = new Vector3(1, 1, 0.0f) ;
+            transform.position = Vector3.MoveTowards((transform.position+=vettoreMovimento), player.transform.position, movementIncrease * Time.deltaTime);
+
+
+        }
+    }
+}
