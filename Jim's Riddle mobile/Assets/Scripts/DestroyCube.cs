45ae6fcceeb7b5290243434a1503a06a9d47a802
framain <63399271+framain@users.noreply.github.com>
2020-11-13 17:45:00 +0100

    Correzione bug

---
+++ Jim's Riddle mobile/Assets/Scripts/DestroyCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Arrow")){
+            Destroy(gameObject);
+        }
+    }
+}
