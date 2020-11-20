a1adb699dcb04630d16b9afd75b6b469cad1dcdc
framain <63399271+framain@users.noreply.github.com>
2020-11-20 14:02:13 +0100

    Aggiunta porta al cortile
    La porta si attiva quando il player collide con un quad invisibile

---
+++ Jim's Riddle mobile/Assets/Scripts/AttivazionePorta.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttivazionePorta : MonoBehaviour
+{
+    public GameObject player, porta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(true);
+        porta.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            Debug.Log("Hai chiuso la porta!");
+            //fai partire l'animazione della porta (da aggiungere)
+            
+          
+            porta.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+    
+}
