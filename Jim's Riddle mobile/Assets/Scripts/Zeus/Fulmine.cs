0f1dd4bc729b3690294dc7768c88b67c06b06f62
framain <63399271+framain@users.noreply.github.com>
2020-11-23 17:23:53 +0100

    Correzione bug

--- Jim's Riddle mobile/Assets/Scripts/Zeus/Fulmine.cs
+++ Jim's Riddle mobile/Assets/Scripts/Zeus/Fulmine.cs
@@ -8,11 +8,9 @@ public class Fulmine : MonoBehaviour
     public float speed;
     public GameObject Arrow1;
     private Vector3 vettoreMovimentoFulmine;
-    private Vector3 add;
     // Start is called before the first frame update
     void Start()
     {
-        add = new Vector3(10f, 10f, 0f);
         vettoreMovimentoFulmine = GameObject.FindGameObjectWithTag("Player").transform.position; /* Ho messo questo vettore nella start in modo che prenda le coordinate attuali.
                                                                                                   * Se lo avessi messo nella update si sarebbero aggiornate in continuazione le coordinate
                                                                                                   * e il player non poteva scappare dal colpo */
@@ -23,23 +21,19 @@ public class Fulmine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, getNormalize(vettoreMovimentoFulmine) * 1000f , speed * Time.deltaTime); //Funzione che da un vettore di partenza insegue un altro vettore
+        transform.position = Vector3.MoveTowards(transform.position, vettoreMovimentoFulmine, speed * Time.deltaTime); //Funzione che da un vettore di partenza insegue un altro vettore
 
     }
 
     public void OnTriggerEnter2D (Collider2D coll)
     {
-            distruggiFulmine(0f);        
+            distruggiFulmine();        
     }
 
-    public Vector3 getNormalize(Vector3 v)
+  
+    void distruggiFulmine()
     {
-        v.Normalize();
-        return v;
-    }
-    void distruggiFulmine(float dopoQuanto)
-    {
-        Destroy(gameObject, dopoQuanto);
+        Destroy(gameObject);
     }
 
 }
