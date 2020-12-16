fc9d58d0e492055cc5a77ad913457f608f878214
framain <63399271+framain@users.noreply.github.com>
2020-12-16 21:10:56 +0100

    Aggiunta la scena FrancescoAnubi
    Anubi genera scheletri ogni volta che viene colpito: gli scheletri seguono il player e gli levano vita. [DA MODIFICARE]

---
+++ Jim's Riddle mobile/Assets/Scripts/Anubi/AnubiAttack.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnubiAttack : MonoBehaviour
+{
+    public Healthbar healtBarAnubi;
+    public GameObject scheletroPrefab;
+    public GameObject spawnPoint1, spawnPoint2, spawnPoint3, spawnPoint4;
+    public HealthbarPlayer healthBarPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Arrow")
+        {
+            CreaScheletro();
+        }
+    }
+
+    public void CreaScheletro()
+    {
+        if (healthBarPlayer.health > 0)
+        {
+            if (healtBarAnubi.health >= 90 && healtBarAnubi.health < 100)
+            {
+                GameObject scheletroInstantiated1 = Instantiate(scheletroPrefab, spawnPoint1.transform);
+            }
+            if (healtBarAnubi.health >= 80 && healtBarAnubi.health < 90)
+            {
+
+                GameObject scheletroInstantiated2 = Instantiate(scheletroPrefab, spawnPoint2.transform);
+
+            }
+            if (healtBarAnubi.health >= 70 && healtBarAnubi.health < 80)
+            {
+
+                GameObject scheletroInstantiated3 = Instantiate(scheletroPrefab, spawnPoint3.transform);
+
+                GameObject scheletroInstantiated2 = Instantiate(scheletroPrefab, spawnPoint2.transform);
+
+                GameObject scheletroInstantiated1 = Instantiate(scheletroPrefab, spawnPoint1.transform);
+
+            }
+            if (healtBarAnubi.health <= 60)
+            {
+
+                GameObject scheletroInstantiated4 = Instantiate(scheletroPrefab, spawnPoint4.transform);
+
+                GameObject scheletroInstantiated3 = Instantiate(scheletroPrefab, spawnPoint3.transform);
+
+                GameObject scheletroInstantiated2 = Instantiate(scheletroPrefab, spawnPoint2.transform);
+
+                GameObject scheletroInstantiated1 = Instantiate(scheletroPrefab, spawnPoint1.transform);
+
+            }
+        }
+      
+    }
+
+
+}
---
+++ Jim's Riddle mobile/Assets/Scripts/Anubi/HealtBarAnubi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealtBarAnubi : MonoBehaviour
+{
+
+    public GameObject healthBarAnubi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBarAnubi.SetActive(false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Arrow")
+        {
+            healthBarAnubi.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
